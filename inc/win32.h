4161d68477c882f7e4cb90fb401a74d78bf4c028
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-04-03 20:28:05 -0700

    wait2e() macro for WaitForSingleObject and WaitForMultipleObjects index to errno_t conversion

--- inc/win32.h
+++ inc/win32.h
@@ -20,4 +20,25 @@
 
 #define b2e(call) (call ? 0 : GetLastError()) // BOOL -> errno_t
 
+// inline errno_t wait2e(DWORD ix) { // rather huge switch statement 0xFFFFFFFF
+//     switch (ix) {
+//         case WAIT_OBJECT_0 : return 0;
+//         case WAIT_ABANDONED: return ERROR_REQUEST_ABORTED;
+//         case WAIT_TIMEOUT  : return ERROR_TIMEOUT;
+//         case WAIT_FAILED   : return runtime.err();
+//         default: // assert(false, "unexpected: %d", r);
+//                  return ERROR_INVALID_HANDLE;
+//     }
+// }
+
+#define wait2e(ix) (errno_t)                                                     \
+    ((int32_t)WAIT_OBJECT_0 <= (int32_t)(ix) && (ix) <= WAIT_OBJECT_0 + 63 ? 0 : \
+      ((ix) == WAIT_ABANDONED ? ERROR_REQUEST_ABORTED :                          \
+        ((ix) == WAIT_TIMEOUT ? ERROR_TIMEOUT :                                  \
+          ((ix) == WAIT_FAILED) ? (errno_t)GetLastError() : ERROR_INVALID_HANDLE \
+        )                                                                        \
+      )                                                                          \
+    )
+
+
 #endif // WIN32
