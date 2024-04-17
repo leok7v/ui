7a07b52f626996b7b6813e6dc81c9735970381a4
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-04-16 18:14:03 -0700

    better version files placement

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -2,4 +2,5 @@
 rmdir /s /q ..\bin
 rmdir /s /q ..\build
 rmdir /s /q ..\msvc2022\.vs
+del /q ..\inc\ut\version.h
 
--- scripts/prebuild.bat
+++ scripts/prebuild.bat
@@ -1,11 +1,11 @@
 @echo off
 pushd ..
 if exist bin\release\version.exe (
-    bin\release\version.exe > build/version.h 2>nul
+    bin\release\version.exe > inc/ut/version.h 2>nul
     goto version_done
 )
 if exist bin\debug\version.exe (
-    bin\debug\version.exe > build/version.h 2>nul
+    bin\debug\version.exe > inc/ut/version.h 2>nul
     goto version_done
 )
 :version_done
