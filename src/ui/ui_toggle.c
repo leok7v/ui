37d2cddeb6b7c0755f9be97554d7311c67efc955
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-07-03 23:19:35 -0700

    better toggle rendering on low dpi screens

--- src/ui/ui_toggle.c
+++ src/ui/ui_toggle.c
@@ -1,41 +1,40 @@
 #include "ut/ut.h"
 #include "ui/ui.h"
 
-static int32_t ui_toggle_paint_on_off(ui_view_t* v, int32_t x, int32_t y) {
+static void ui_toggle_paint_on_off(ui_view_t* v) {
+    ui_ltrb_t i = ui_view.gaps(v, &v->insets);
+    int32_t x = v->x;
+    int32_t y = v->y + i.top;
     ui_color_t c = ui_colors.darken(v->background,
         !ui_theme.is_app_dark() ? 0.125f : 0.5f);
     ui_color_t b = v->state.pressed ? ui_colors.tone_green : c;
-//  const int32_t bl = v->fm->baseline;
-//  const int32_t xl = bl - v->fm->x_height;
-    const int32_t a  = v->fm->ascent;
-    const int32_t d  = v->fm->descent;
-    const int32_t w  = v->fm->em.w;
+    const int32_t a = v->fm->ascent;
+    const int32_t d = v->fm->descent;
+    const int32_t w = v->fm->em.w;
     int32_t h = a + d;
     int32_t r = (h / 2) | 0x1; // must be odd
     h = r * 2 + 1;
-//  y += bl - h;
-
+    y += (v->h - i.top - i.bottom - h + 1) / 2;
+    y += r;
     x += r;
-    int32_t y1 = y + h - r + 2;
     ui_color_t border = ui_theme.is_app_dark() ?
         ui_colors.darken(v->color, 0.5) :
         ui_colors.lighten(v->color, 0.5);
     if (v->state.hover) {
         border = ui_colors.get_color(ui_color_id_hot_tracking);
     }
-    ui_gdi.circle(x, y1, r, border, b);
-    ui_gdi.circle(x + w - r, y1, r, border, b);
-    ui_gdi.fill(x, y1 - r, w - r + 1, h, b);
-    ui_gdi.line(x, y1 - r, x + w - r + 1, y1 - r, border);
-    ui_gdi.line(x, y1 + r, x + w - r + 1, y1 + r, border);
+    ui_gdi.circle(x, y, r, border, b);
+    ui_gdi.circle(x + w - r, y, r, border, b);
+    ui_gdi.fill(x, y - r, w - r + 1, h, b);
+    ui_gdi.line(x, y - r, x + w - r + 1, y - r, border);
+    ui_gdi.line(x, y + r, x + w - r + 1, y + r, border);
     int32_t x1 = v->state.pressed ? x + w - r : x;
     // circle is too bold in control color - water it down
     ui_color_t fill = ui_theme.is_app_dark() ?
         ui_colors.darken(v->color, 0.5f) : ui_colors.lighten(v->color, 0.5f);
     border = ui_theme.is_app_dark() ?
         ui_colors.darken(fill, 0.0625f) : ui_colors.lighten(fill, 0.0625f);
-    ui_gdi.circle(x1, y1, r - 2, border, fill);
-    return x + w + 2;
+    ui_gdi.circle(x1, y, r - 2, border, fill);
 }
 
 static const char* ui_toggle_on_off_label(ui_view_t* v,
@@ -65,10 +64,7 @@ static void ui_toggle_paint(ui_view_t* v) {
     ui_view_text_metrics_t tm = {0};
     ui_view.text_measure(v, text, &tm);
     ui_view.text_align(v, &tm);
-    ui_ltrb_t i = ui_view.gaps(v, &v->insets);
-    const int32_t tx = v->x;
-    const int32_t ty = v->y + i.top + tm.xy.y;
-    ui_toggle_paint_on_off(v, tx, ty);
+    ui_toggle_paint_on_off(v);
     const ui_color_t text_color = !v->state.hover ? v->color :
             (ui_theme.is_app_dark() ? ui_colors.white : ui_colors.black);
     const ui_gdi_ta_t ta = { .fm = v->fm, .color = text_color };
