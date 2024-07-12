3480144de646ab3b2a9c9feca2c290aa4f35df57
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-07-11 19:29:30 -0700

    ui_edit: scrollup fix and better word selection on double click, paragraph on long press

--- src/ui/ui_edit_view.c
+++ src/ui/ui_edit_view.c
@@ -516,6 +516,9 @@ static int32_t ui_edit_glyph_width_px(ui_edit_t* e, const ui_edit_pg_t pg) {
 // xy_to_pg() (x,y) (0,0, width x height) -> paragraph # glyph #
 
 static ui_edit_pg_t ui_edit_xy_to_pg(ui_edit_t* e, int32_t x, int32_t y) {
+// TODO: remove
+//  const ui_ltrb_t i = ui_view.margins(&e->view, &e->view.insets);
+//  traceln("x,y: %d,%d insets left:%d right:%d", x, y, i.left, i.right);
     ui_edit_text_t* dt = &e->doc->text; // document text
     ui_edit_pg_t pg = {-1, -1};
     int32_t py = 0; // paragraph `y' coordinate
@@ -531,14 +534,18 @@ static ui_edit_pg_t ui_edit_xy_to_pg(ui_edit_t* e, int32_t x, int32_t y) {
                 int32_t w = ui_edit_text_width(e, s, r->bytes);
                 pg.pn = i;
                 if (x >= w) {
-                    const int32_t last_run = j == runs - 1;
-                    pg.gp = r->gp + ut_max(0, r->glyphs - 1 + last_run);
+                    pg.gp = r->gp + r->glyphs;
                 } else {
                     pg.gp = r->gp + ui_edit_glyph_at_x(e, i, j, x);
+// TODO: remove
+//                  traceln("pg.gp: %d r->gp: %d ui_edit_glyph_at_x(%d, %d, x:%d)",
+//                          pg.gp, r->gp, i, j, x, ui_edit_glyph_at_x(e, i, j, x));
                     if (pg.gp < r->glyphs - 1) {
                         ui_edit_pg_t right = {pg.pn, pg.gp + 1};
                         int32_t x0 = ui_edit_pg_to_xy(e, pg).x;
                         int32_t x1 = ui_edit_pg_to_xy(e, right).x;
+// TODO: remove
+//                      traceln("x0: %d x1: %d", x0, x1);
                         if (x1 - x < x - x0) {
                             pg.gp++; // snap to closest glyph's 'x'
                         }
@@ -550,6 +557,8 @@ static ui_edit_pg_t ui_edit_xy_to_pg(ui_edit_t* e, int32_t x, int32_t y) {
         }
         if (py > e->h) { break; }
     }
+// TODO: remove
+//  traceln("x,y: %d,%d p:d %d:%d", x, y, pg.pn, pg.gp);
     return pg;
 }
 
@@ -558,8 +567,15 @@ static void ui_edit_set_caret(ui_edit_t* e, int32_t x, int32_t y) {
         if (e->focused && ui_app.focused()) {
             ui_app.move_caret(e->x + x, e->y + y);
         }
+        const ui_ltrb_t i = ui_view.margins(&e->view, &e->insets);
+        // caret in i.left .. e->view.w - i.right
+        //          i.top  .. e->view.h - i.bottom
+        // coordinate space
+        assert(i.left <= x && x < e->w && i.top <= y && y < e->h);
         e->caret.x = x;
         e->caret.y = y;
+// TODO: remove
+//      traceln("caret: %d, %d", x, y);
     }
 }
 
@@ -1095,9 +1111,11 @@ static void ui_edit_select_word(ui_edit_t* e, int32_t x, int32_t y) {
         }
         if (glyph.bytes > 0 && *glyph.s > 0x20) {
             ui_edit_pg_t from = p;
+            char first_ascii = 0x00;
             while (from.gp > 0) {
                 from.gp--;
                 ui_edit_glyph_t g = ui_edit_glyph_at(e, from);
+                first_ascii = glyph.bytes == 1 ? *glyph.s : 0x00;
                 if (g.bytes == 0 || *g.s <= 0x20) {
                     from.gp++;
 //                  traceln("left while space @%d 0x%02X", from.gp, *g.s);
@@ -1109,7 +1127,9 @@ static void ui_edit_select_word(ui_edit_t* e, int32_t x, int32_t y) {
             while (to.gp < glyphs) {
                 to.gp++;
                 ui_edit_glyph_t g = ui_edit_glyph_at(e, to);
-                if (g.bytes == 0 || *g.s <= 0x20) {
+                const bool starts_with_alnum = isalnum(first_ascii);
+                bool stop = starts_with_alnum && g.bytes == 1 && !isalnum(*g.s);
+                if (g.bytes == 0 || *g.s <= 0x20 || stop) {
 //                  traceln("right while space @%d 0x%02X", to.gp, *g.s);
                     break;
                 }
@@ -1149,14 +1169,18 @@ static void ui_edit_select_paragraph(ui_edit_t* e, int32_t x, int32_t y) {
 }
 
 static void ui_edit_click(ui_edit_t* e, int32_t x, int32_t y) {
+    // x, y in 0..e->w, 0->e.h coordinate space
+    assert(0 <= x && x < e->w && 0 <= y && y < e->h);
     ui_edit_text_t* dt = &e->doc->text; // document text
-    ui_edit_pg_t p = ui_edit_xy_to_pg(e, x, y);
-    if (0 <= p.pn && 0 <= p.gp && ui_view.has_focus(&e->view)) {
-        swear(dt->np > 0 && p.pn < dt->np);
-        int32_t glyphs = ui_edit_glyphs_in_paragraph(e, p.pn);
-        if (p.gp > glyphs) { p.gp = ut_max(0, glyphs); }
+    ui_edit_pg_t pg = ui_edit_xy_to_pg(e, x, y);
+//  TODO: remove
+//  traceln("x,y: %d,%d p:d %d:%d", e->caret.x, e->caret.y, pg.pn, pg.gp);
+    if (0 <= pg.pn && 0 <= pg.gp && ui_view.has_focus(&e->view)) {
+        swear(dt->np > 0 && pg.pn < dt->np);
+        int32_t glyphs = ui_edit_glyphs_in_paragraph(e, pg.pn);
+        if (pg.gp > glyphs) { pg.gp = ut_max(0, glyphs); }
 //      traceln("move_caret: %d.%d", p.pn, p.gp);
-        ui_edit_move_caret(e, p);
+        ui_edit_move_caret(e, pg);
     }
 }
 
@@ -1175,6 +1199,8 @@ static bool ui_edit_tap(ui_view_t* v, int32_t unused(ix), bool pressed) {
     const int32_t x = ui_app.mouse.x - (v->x + e->inside.left);
     const int32_t y = ui_app.mouse.y - (v->y + e->inside.top);
     bool inside = 0 <= x && x < e->w && 0 <= y && y < e->h;
+//  TODO: remove
+//  traceln("mouse: %d,%d x,y: %d %d inside: %d", ui_app.mouse.x, ui_app.mouse.y, x, y, inside);
     if (inside) {
         if (pressed) {
             e->edit.buttons = 0;
@@ -1193,7 +1219,7 @@ static bool ui_edit_long_press(ui_view_t* v, int32_t unused(ix)) {
     const int32_t y = ui_app.mouse.y - (v->y + e->inside.top);
     bool inside = 0 <= x && x < e->w && 0 <= y && y < e->h;
     if (inside && ui_edit_range.is_empty(e->selection)) {
-        ui_edit_select_word(e, x, y);
+        ui_edit_select_paragraph(e, x, y);
     }
     return true;
 }
@@ -1204,7 +1230,7 @@ static bool ui_edit_double_tap(ui_view_t* v, int32_t unused(ix)) {
     const int32_t y = ui_app.mouse.y - (v->y + e->inside.top);
     bool inside = 0 <= x && x < e->w && 0 <= y && y < e->h;
     if (inside && e->selection.a[0].pn == e->selection.a[1].pn) {
-        ui_edit_select_paragraph(e, x, y);
+        ui_edit_select_word(e, x, y);
     }
     return false;
 }
@@ -1229,9 +1255,16 @@ static void ui_edit_mouse_scroll(ui_view_t* v, ui_point_t dx_dy) {
 //        To me back forward stack navigation is much more intuitive and
 //        much mode "modeless" in spirit of cut/copy/paste. But opinions
 //        and editing habits vary. Easy to implement.
-            ui_edit_pg_t pg = ui_edit_xy_to_pg(e, e->caret.x, e->caret.y);
+            const int32_t x = e->caret.x - e->inside.left;
+            const int32_t y = e->caret.y - e->inside.top;
+            ui_edit_pg_t pg = ui_edit_xy_to_pg(e, x, y);
+// TODO: remove
+//          traceln("x,y: %d,%d caret: %d,%d p:d %d:%d", x, y, e->caret.x, e->caret.y, pg.pn, pg.gp);
             if (pg.pn >= 0 && pg.gp >= 0) {
+                assert(pg.gp <= e->doc->text.ps[pg.pn].g);
                 ui_edit_move_caret(e, pg);
+            } else {
+                ui_edit_click(e, x, y);
             }
         }
     }
