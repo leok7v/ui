8dc30b07741deb0a110e3fd8b435d529cc2cde85
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-07-24 00:26:57 -0700

    I am groot quote.

--- src/samples/groot/groot.c
+++ src/samples/groot/groot.c
@@ -7,23 +7,24 @@
 
 // TODO: stack(ui_text with the content "I am groot..", view_groot)
 // Top: Find single line edit control "groot" with Find button that selects found text
-// bottom: UTC time and local time status of all views?, debug toggles (text metric and margins)
+// bottom: UTC time and local time status of all views?,
+// Right view: debug toggles (text metric and margins) + message box on close window:
+// "Groot: I am groot...\n"
+// "Rocket: He says: Where the heck are you going now?\n"
+// "Retry" "Abort" "Ignore"
+
 
 enum { width = 512, height = 512 };
 
 static uint8_t gs[width * height]; // greyscale
-
-static ui_view_t  list         = ui_view(list);
-static ui_label_t label_left   = ui_label(0, "Left");
-static ui_label_t label_right  = ui_label(0, "Right");
-static ui_label_t label_top    = ui_label(0, "Top");
-static ui_label_t label_bottom = ui_label(0, "Bottom");
-
-static ui_image_t image; // grayscale image
+//static ui_image_t image; // grayscale image
 
 static ui_iv_t view_groot;
 static ui_iv_t view_rocket;
 static ui_iv_t view_gs[2]; // two views at the same image
+
+static ui_edit_t     view_text;
+static ui_edit_doc_t document;
 
 static void* load_image(const uint8_t* data, int64_t bytes, int32_t* w, int32_t* h,
     int32_t* bpp, int32_t preferred_bytes_per_pixel) {
@@ -69,7 +70,7 @@ static void gs_erase(ui_view_t* v) {
 
 static void slider_format(ui_view_t* v) {
     ui_slider_t* s = (ui_slider_t*)v;
-    ui_view.set_text(v, "%d", s->value);
+    ui_view.set_text(v, "%.0f%%", s->value * 100.0 / 255.0);
 }
 
 static void slider_callback(ui_view_t* v) {
@@ -78,26 +79,63 @@ static void slider_callback(ui_view_t* v) {
 //  ut_println("value: %d", slider->value);
 }
 
+static void init_images(void) {
+    ui_iv.init(&view_groot);
+    init_image(&view_groot.image, groot, ut_countof(groot));
+    // view of groot image:
+    ui_iv.ratio(&view_groot, 4, 1); // 4:1
+    view_groot.alpha = 0.5;
+    view_groot.padding = (ui_margins_t){0.125f, 0.125f, 0.125f, 0.125f};
+    view_groot.focusable = false; // because it is stacked under text editor
+    // view of rocket image:
+    ui_iv.init(&view_rocket);
+    init_image(&view_rocket.image, rocket, ut_countof(rocket));
+    ui_iv.ratio(&view_rocket, 3, 1); // 3:1
+    view_rocket.padding = (ui_margins_t){0.125f, 0.125f, 0.125f, 0.125f};
+    view_groot.focusable = false; // no zoom/pan
+    init_gs();
+}
+
+static void init_text(void) {
+    ut_swear(ui_edit_doc.init(&document,
+    "Star-Lord: \"What is wrong with giving tree, here?\"\n"
+    "Rocket: \"Well, he don't know talking good like me and you, "
+              "so his vocabulistics is limited to 'I' and 'am' and 'Groot.' "
+              "Exclusively, in that order.\"", -1, false));
+    ui_edit.init(&view_text, &document);
+    view_text.hide_word_wrap = true;
+    view_text.padding = (ui_margins_t){0};
+    view_text.insets = (ui_margins_t){0};
+    view_text.background_id = 0;
+    view_text.background = ui_colors.transparent;
+    ut_str_printf(view_text.hint,
+        "Text Edit:\n\n"
+        "Try double clicking to select a word\n"
+        "or long-pressing to select a paragraph.\n\n"
+        "Ctrl+[Shift]+arrows, Ctrl+X|C|V,\n"
+        "Undo/Redo Ctrl+Z/Y\n"
+    );
+}
+
 static ui_view_t* align(ui_view_t* v, int32_t align) {
     v->align = align;
     return v;
 }
 
+static ui_view_t* fill_parent(ui_view_t* v) {
+    v->max_h  = ui.infinity;
+    v->max_w  = ui.infinity;
+    return v;
+}
+
 static void opened(void) {
-    ui_iv.init(&view_groot);
-    init_image(&view_groot.image, groot, ut_countof(groot));
-    // TODO: ui_iv.scale(2,1)
-    view_groot.zoom = 3; view_groot.zn = 2; view_groot.zd = 1; // 2:1
-    view_groot.alpha = 0.5;
-    view_groot.padding = (ui_margins_t){0.125f, 0.125f, 0.125f, 0.125f};
-    ui_iv.init(&view_rocket);
-    init_image(&view_rocket.image, rocket, ut_countof(rocket));
-    // TODO: ui_iv.scale(2,1)
-    view_rocket.zoom = 3; view_rocket.zn = 2; view_rocket.zd = 1; // 2:1
-    view_rocket.padding = (ui_margins_t){0.125f, 0.125f, 0.125f, 0.125f};
-    init_gs();
-    // painting greyscale pixels w/o pushing them into device
-    // bitmap:
+    init_images();
+    init_text();
+    static ui_view_t  list         = ui_view(list);
+    static ui_label_t label_left   = ui_label(0, "Left");
+    static ui_label_t label_top    = ui_label(0, "Top");
+    static ui_label_t label_bottom = ui_label(0, "Bottom");
+    // painting greyscale pixels will be handled w/o device bitmap:
     for (int32_t i = 0; i < ut_countof(view_gs); i++) {
         ui_iv.init_with(&view_gs[i], gs, width, height, 1, width);
         view_gs[i].erase = gs_erase;
@@ -107,22 +145,27 @@ static void opened(void) {
     static ui_view_t   center = ui_view(span);
     static ui_view_t   left   = ui_view(list);
     static ui_view_t   right  = ui_view(list);
+    static ui_view_t   stack  = ui_view(stack);
     static ui_view_t   bottom = ui_view(stack);
     static ui_view_t   spacer = ui_view(spacer);
-    static ui_slider_t slider = ui_slider("128", 8.0f, 0, 255,
+    static ui_slider_t slider = ui_slider("128", 16.0f, 0, 255,
             slider_format, slider_callback);
     slider.value = 128;
-    ui_view.add(&left,
+    ui_view.add(fill_parent(&left),
             align(&view_gs[0].view, ui.align.left),
             align(&view_gs[1].view, ui.align.left),
             null
     );
-    ui_view.add(&right,
-                &label_right,
-                &spacer,
-                &slider,
+    ui_view.add(&stack,
                 &view_groot,
+                fill_parent(&view_text.view),
+                null
+    );
+    ui_view.add(&right,
+                &spacer,
                 &view_rocket,
+                &slider,
+                &stack,
                 null
     );
     ui_view.add(&top,    &label_top,    null);
@@ -131,10 +174,8 @@ static void opened(void) {
                 align(&left,  ui.align.top),
                 align(&right, ui.align.top),
                 null);
-    left.max_h  = ui.infinity;
-    left.max_w  = ui.infinity;
     ui_view.add(ui_app.content,
-        ui_view.add(&list,
+        ui_view.add(fill_parent(&list),
             align(&top,    ui.align.center),
             align(&center, ui.align.left),
             align(&bottom, ui.align.center),
@@ -143,8 +184,6 @@ static void opened(void) {
         null
     );
 //  list.debug.paint.margins = true;
-    list.max_w  = ui.infinity;
-    list.max_h  = ui.infinity;
     center.insets  = (ui_margins_t){0};
     center.padding = (ui_margins_t){0};
     static ui_view_t* panels[] = { &top, &left, &right, &bottom  };
@@ -165,9 +204,7 @@ static void opened(void) {
     });
     center.max_h = ui.infinity;
     for (int32_t i = 0; i < ut_countof(view_gs); i++) {
-        view_gs[i].erase = panel_erase;
-        view_gs[i].max_h = ui.infinity;
-        view_gs[i].max_w = ui.infinity;
+        fill_parent(&view_gs[i].view)->erase = panel_erase;
     }
     view_gs[0].padding.bottom = 0.25f;
     view_gs[1].padding.top    = 0.25f;
@@ -191,8 +228,8 @@ ui_app_t ui_app = {
     .opened = opened,
     .closed = closed,
     .window_sizing = { // inches
-        .min_w = 5.0f,
-        .min_h = 4.0f,
+        .min_w = 7.0f,
+        .min_h = 6.0f,
         .ini_w = 11.0f,
         .ini_h = 7.0f
     }
