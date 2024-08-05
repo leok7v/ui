84f88f54a4c960002b3825fc063de9848b53cef2
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-08-05 15:41:02 -0700

    dancing groot moved animation to dispatch thread

--- src/samples/sample6.c
+++ src/samples/sample6.c
@@ -16,16 +16,19 @@ static struct {
 
 enum { max_speed = 3 };
 
-static struct {
+typedef struct animation_s {
     int32_t  index; // animation index 0..gif.frames - 1
-    rt_event_t  quit;
-    rt_thread_t thread;
     uint32_t seed; // for rt_num.random32()
     int32_t  x;
     int32_t  y;
+    int32_t  w;
+    int32_t  h;
     int32_t  speed_x;
     int32_t  speed_y;
-} animation;
+} animation_t;
+
+static animation_t animation;
+static rt_thread_t thread; // gif loader thread
 
 static bool   muted;
 static fp64_t volume; // be mute
@@ -59,18 +62,13 @@ static const char* midi_file(void) {
         rt_assert(filename[0] != 0);
         int64_t written = 0;
         rt_fatal_if_error(rt_files.write_fully(filename, data, bytes,
-                                                        &written));
+                                              &written));
         rt_assert(written == bytes);
     }
     return filename;
 }
 
 static void paint(ui_view_t* v) {
-    if (animation.x < 0 && animation.y < 0) {
-        animation.x = (v->w - gif.w) / 2;
-        animation.y = (v->h - gif.h) / 2;
-    }
-    ui_gdi.fill(0, 0, v->w, v->h, ui_colors.black);
     int32_t w = rt_min(v->w, background.w);
     int32_t h = rt_min(v->h, background.h);
     int32_t x = (v->w - w) / 2;
@@ -82,9 +80,10 @@ static void paint(ui_view_t* v) {
         uint8_t* p = gif.pixels + gif.w * gif.h * gif.bpp * animation.index;
         ui_bitmap_t frame = { 0 };
         ui_gdi.bitmap_init(&frame, gif.w, gif.h, gif.bpp, p);
-        x = animation.x - gif.w / 2;
-        y = animation.y - gif.h / 2;
-        ui_gdi.alpha(x, y, gif.w, gif.h, 0,0, frame.w, frame.h, &frame, 1.0);
+        x = animation.x - animation.w / 2;
+        y = animation.y - animation.h / 2;
+        ui_gdi.alpha(x, y, animation.w, animation.h, 0, 0,
+                    frame.w, frame.h, &frame, 1.0);
         ui_gdi.bitmap_dispose(&frame);
     }
     ui_gdi_ta_t ta = ui_gdi.ta.prop.H1;
@@ -184,65 +183,78 @@ static void load_gif(void) {
     // load_animated_gif() calls realloc(delays) w/o first alloc()
     r = rt_heap.allocate(null, (void**)&gif.delays, sizeof(int32_t), false);
     rt_swear(r == 0 && gif.delays != null);
+    // gif.pixels assignment is "atomic"
     gif.pixels = load_animated_gif(data, bytes, &gif.delays,
         &gif.w, &gif.h, &gif.frames, &gif.bpp, 4);
-    if (gif.pixels == null || gif.bpp != 4 || gif.frames < 1) {
-        rt_println("%s", stbi_failure_reason());
-    }
-    rt_fatal_if(gif.pixels == null || gif.bpp != 4 || gif.frames < 1);
+    rt_swear(gif.pixels != null && gif.bpp == 4 && gif.frames >= 1,
+             "%s", stbi_failure_reason());
     // resources cannot be unmapped do not call rt_mem.unmap()
 }
 
-static void animate(void) {
-    for (;;) {
-        ui_app.request_redraw();
-        fp64_t delay = gif.delays[animation.index] * 0.001; // seconds
-        if (rt_event.wait_or_timeout(animation.quit, delay) == 0) {
-            break;
+static void animation_step(rt_work_t* work);
+
+static void schedule_next_step(rt_work_t* work) {
+    rt_swear(0 <= animation.index && animation.index < gif.frames);
+    // milliseconds to seconds:
+    fp64_t ds = gif.delays[animation.index] * 0.001; // delay in seconds
+    work->when = rt_clock.seconds() + ds;
+    ui_app.post(work);
+}
+
+static void animation_step(rt_work_t* work) {
+    rt_swear(rt_thread.id() == ui_app.tid);
+    int32_t multiplier = 1;
+    while (gif.w * multiplier < ui_app.crc.w / 2 &&
+           gif.h * multiplier < ui_app.crc.h / 2) {
+        multiplier++;
+    }
+    animation_t* a = &animation; // shorthand naming
+    a->w = gif.w * multiplier;
+    a->h = gif.h * multiplier;
+    if (a->x < 0 && a->y < 0) {
+        a->x = (ui_app.crc.w - a->w) / 2;
+        a->y = (ui_app.crc.h - a->h) / 2;
+    }
+//  rt_println("%d %d speed: %d %d", a->x, a->y,
+//                                   a->speed_x,
+//                                   a->speed_y);
+    a->index = (a->index + 1) % gif.frames;
+    while (a->speed_x == 0) {
+        const uint32_t r = rt_num.random32(&a->seed);
+        a->speed_x = r % (max_speed * 2 + 1) - max_speed;
+    }
+    while (a->speed_y == 0) {
+        const uint32_t r = rt_num.random32(&a->seed);
+        a->speed_y = r % (max_speed * 2 + 1) - max_speed;
+    }
+    a->x += a->speed_x;
+    a->y += a->speed_y;
+    if (a->x - a->w / 2 < 0) {
+        a->x = a->w / 2;
+        a->speed_x = -a->speed_x;
+    } else if (a->x + a->w / 2 >= ui_app.root->w) {
+        a->x = ui_app.root->w - a->w / 2 - 1;
+        a->speed_x = -a->speed_x;
+    }
+    if (a->y - a->h / 2 < 0) {
+        a->y = a->h / 2;
+        a->speed_y = -a->speed_y;
+    } else if (a->y + a->h / 2 >= ui_app.root->h) {
+        a->y = ui_app.root->h - a->h / 2 - 1;
+        a->speed_y = -a->speed_y;
+    }
+    int inc = rt_num.random32(&a->seed) % 2 == 0 ? -1 : +1;
+    if (rt_num.random32(&a->seed) % 2 == 0) {
+        if (1 <= a->speed_x + inc && a->speed_x + inc < max_speed) {
+            a->speed_x += inc;
         }
-        if (animation.x >= 0 && animation.y >= 0) {
-//          rt_println("%d %d speed: %d %d", animation.x, animation.y,
-//                                           animation.speed_x,
-//                                           animation.speed_y);
-            animation.index = (animation.index + 1) % gif.frames;
-            while (animation.speed_x == 0) {
-                animation.speed_x = rt_num.random32(&animation.seed) %
-                                    (max_speed * 2 + 1) - max_speed;
-            }
-            while (animation.speed_y == 0) {
-                animation.speed_y = rt_num.random32(&animation.seed) %
-                                    (max_speed * 2 + 1) - max_speed;
-            }
-            animation.x += animation.speed_x;
-            animation.y += animation.speed_y;
-            if (animation.x - gif.w / 2 < 0) {
-                animation.x = gif.w / 2;
-                animation.speed_x = -animation.speed_x;
-            } else if (animation.x + gif.w / 2 >= ui_app.root->w) {
-                animation.x = ui_app.root->w - gif.w / 2 - 1;
-                animation.speed_x = -animation.speed_x;
-            }
-            if (animation.y - gif.h / 2 < 0) {
-                animation.y = gif.h / 2;
-                animation.speed_y = -animation.speed_y;
-            } else if (animation.y + gif.h / 2 >= ui_app.root->h) {
-                animation.y = ui_app.root->h - gif.h / 2 - 1;
-                animation.speed_y = -animation.speed_y;
-            }
-            int inc = rt_num.random32(&animation.seed) % 2 == 0 ? -1 : +1;
-            if (rt_num.random32(&animation.seed) % 2 == 0) {
-                if (1 <= animation.speed_x + inc &&
-                    animation.speed_x + inc < max_speed) {
-                    animation.speed_x += inc;
-                }
-            } else {
-                if (1 <= animation.speed_y + inc &&
-                    animation.speed_y + inc < max_speed) {
-                    animation.speed_y += inc;
-                }
-            }
+    } else {
+        if (1 <= a->speed_y + inc && a->speed_y + inc < max_speed) {
+            a->speed_y += inc;
         }
     }
+    ui_app.request_redraw();
+    schedule_next_step(work);
 }
 
 static void animated_gif_loader(void* rt_unused(ignored)) {
@@ -250,7 +262,8 @@ static void animated_gif_loader(void* rt_unused(ignored)) {
     ui_app.set_cursor(ui_app.cursors.wait);
     load_gif();
     ui_app.set_cursor(cursor);
-    animate();
+    static rt_work_t work = { .work = animation_step };
+    schedule_next_step(&work);
 }
 
 static void load_sample_png(void) { // from resources
@@ -277,23 +290,21 @@ static void opened(void) {
     animation.seed = (uint32_t)rt_clock.nanoseconds();
     animation.x = -1;
     animation.y = -1;
-    animation.quit = rt_event.create();
-    animation.thread = rt_thread.start(animated_gif_loader, null);
+    thread = rt_thread.start(animated_gif_loader, null);
     static rt_work_t work = { .work = delayed };
     work.when = rt_clock.seconds() + 0.125;
     ui_app.post(&work);
 }
 
 static void closed(void) {
-    rt_event.set(animation.quit);
-    rt_thread.join(animation.thread, -1);
-    rt_event.dispose(animation.quit);
+    rt_thread.join(thread, -1);
     ui_gdi.bitmap_dispose(&background);
     stbi_image_free(gif.pixels);
     stbi_image_free(gif.delays);
     if (ui_midi.is_open(&midi)) {
-        // restore premuted volume
-        rt_fatal_if_error(ui_midi.set_volume(&midi, volume != 0 ? volume : 0.5));
+        // restore pre-muted volume:
+        rt_fatal_if_error(ui_midi.set_volume(&midi,
+                        volume != 0 ? volume : 0.5));
     }
     stop_and_close();
     delete_midi_file();
@@ -330,8 +341,8 @@ ui_app_t ui_app = {
     .dark_mode = true,
     .init = init,
     .window_sizing = {
-        .min_w =  6.0f,
-        .min_h =  5.0f,
+        .min_w =  4.0f,
+        .min_h =  3.0f,
         .ini_w =  8.0f,
         .ini_h =  6.0f
     }
